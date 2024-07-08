8b67346f4512040c5bc49e25f652e52bcc9e2d90
chenqw <zenkiray@live.com>
2024-07-08 16:45:47 +0800

    -[功能]增加架构图编辑来源
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/process/exception/processtask/ProcessTaskNotFoundException.java
+++ src/main/java/neatlogic/framework/process/exception/processtask/ProcessTaskNotFoundException.java
@@ -2,15 +2,22 @@ package neatlogic.framework.process.exception.processtask;
 
 import neatlogic.framework.process.exception.core.ProcessTaskRuntimeException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProcessTaskNotFoundException extends ProcessTaskRuntimeException {
 
     private static final long serialVersionUID = 2861954159600811000L;
 
-    public ProcessTaskNotFoundException(String processTask) {
-        super("工单：“{0}”不存在", processTask);
+    public ProcessTaskNotFoundException(Long processTaskId) {
+        super("工单“{0}”不存在", processTaskId);
+    }
+
+    public ProcessTaskNotFoundException(List<Long> processTaskIdList) {
+        super("工单“{0}”不存在", processTaskIdList.stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     public ProcessTaskNotFoundException() {
-        super("exception.process.processtasknotfoundexception.1");
+        super("工单不存在");
     }
 }
