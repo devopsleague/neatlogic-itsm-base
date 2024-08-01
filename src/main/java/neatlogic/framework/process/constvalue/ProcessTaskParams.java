0164de60e79083f67c964a88062955241e9f4c5a
1437892690@qq.com <1437892690@qq.com>
2024-08-01 14:54:28 +0800

    [功能] 流程自动处理节点的参数值添加一个上报人用户ID的选项
    关联 #[1213546912776192]流程自动处理节点的参数值添加一个上报人用户ID的选项 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1213546912776192

--- src/main/java/neatlogic/framework/process/constvalue/ProcessTaskParams.java
+++ src/main/java/neatlogic/framework/process/constvalue/ProcessTaskParams.java
@@ -1,64 +1,37 @@
 package neatlogic.framework.process.constvalue;
 
-import neatlogic.framework.util.I18n;
-
 public enum ProcessTaskParams {
-    TASKID("id", new I18n("工单ID")),
-    STEPID("stepid", new I18n("步骤ID")),
-    TITLE("title", new I18n("标题")),
-    CHANNELTYPE("channeltype", new I18n("服务类型")),
-    CONTENT("content", new I18n("上报内容")),
-    STARTTIME("starttime", new I18n("开始时间")),
-    OWNER("owner", new I18n("上报人")),
-    PRIORITY("priority", new I18n("优先级")),
-    OWNERCOMPANY("ownercompany", new I18n("上报人公司")),
-    OWNERDEPARTMENT("ownerdepartment", new I18n("上报人部门")),
-    OWNERROLE("ownerrole", new I18n("上报人角色")),
-    OWNERLEVEL("ownerlevel", new I18n("上报人级别")),
-    STEPTASK("steptask", new I18n("子任务")),
-    ACTIONTRIGGERUSER("actiontriggeruser", new I18n("动作触发人")),
-    STEPTASKID("steptaskid", new I18n("子任务ID"));
+    TASKID("id", "工单ID"),
+    STEPID("stepid", "步骤ID"),
+    TITLE("title", "标题"),
+    CHANNELTYPE("channeltype", "服务类型"),
+    CONTENT("content", "上报内容"),
+    STARTTIME("starttime", "开始时间"),
+    OWNER("owner", "上报人"),
+    OWNERUSERID("owneruserid", "上报人用户ID"),
+    PRIORITY("priority", "优先级"),
+    OWNERCOMPANY("ownercompany", "上报人公司"),
+    OWNERDEPARTMENT("ownerdepartment", "上报人部门"),
+    OWNERROLE("ownerrole", "上报人角色"),
+    OWNERLEVEL("ownerlevel", "上报人级别"),
+    STEPTASK("steptask", "子任务"),
+    ACTIONTRIGGERUSER("actiontriggeruser", "动作触发人"),
+    STEPTASKID("steptaskid", "子任务ID");
 
     private String value;
-    private I18n text;
-//    private ParamType paramType;
-//    private String freemarkerTemplate;
+    private String text;
 
-    ProcessTaskParams(String value, I18n text) {
+    ProcessTaskParams(String value, String text) {
         this.value = value;
         this.text = text;
     }
-
-//    ProcessTaskParams(String value, String text, ParamType paramType) {
-//        this.value = value;
-//        this.text = text;
-//        this.paramType = paramType;
-//    }
-
-//    private ProcessTaskParams(String value, String text, ParamType paramType, String freemarkerTemplate) {
-//        this.value = value;
-//        this.text = text;
-//        this.paramType = paramType;
-//        this.freemarkerTemplate = freemarkerTemplate;
-//    }
 
     public String getValue() {
         return value;
     }
 
     public String getText() {
-        return text.toString();
+        return text;
     }
-
-//    public ParamType getParamType() {
-//        return paramType;
-//    }
-
-//    public String getFreemarkerTemplate() {
-//        if (freemarkerTemplate == null && paramType != null) {
-//            freemarkerTemplate = paramType.getFreemarkerTemplate(value);
-//        }
-//        return freemarkerTemplate;
-//    }
 
 }
