406870fa22f9dbcf0f88b86d36c4a91b3bf14532
lvzk <897706680@qq.com>
2024-08-09 15:52:36 +0800

    [功能]工单中心转交过滤规则调整：点击转交按钮则是转交方，和时间线保持一致

---
+++ src/main/java/neatlogic/framework/process/workcenter/table/ProcessTaskStepCostSqlTable.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.framework.process.workcenter.table;
+
+import neatlogic.framework.util.I18n;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessTaskStepCostSqlTable implements ISqlTable {
+
+    @Override
+    public String getName() {
+        return "processtask_step_cost";
+    }
+
+    @Override
+    public String getShortName() {
+        return "psa";
+    }
+
+    public enum FieldEnum {
+        PROCESSTASK_ID("processtask_id", new I18n("工单ID")),
+        PROCESSTASK_STEP_ID("processtask_step_id", new I18n("工单步骤ID")),
+        START_USER_UUID("start_user_uuid", new I18n("开始操作用户UUID")),
+        START_OPERATE("start_operate", new I18n("开始操作类型"));
+
+        private final String name;
+        private final I18n text;
+
+        FieldEnum(String _value, I18n _text) {
+            this.name = _value;
+            this.text = _text;
+        }
+
+        public String getValue() {
+            return name;
+        }
+
+        public String getText() {
+            return text.toString();
+        }
+    }
+}
